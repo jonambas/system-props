04d92b7cc79b9791db58491be77d6f30de9882f9
jonambas <jon@jonambas.com>
2022-07-11 09:13:42 -0400

    build: add size limit plugin

--- scripts/buildPackage.js
+++ scripts/buildPackage.js
@@ -1,16 +1,6 @@
 #!/usr/bin/env node
 const { build } = require('esbuild');
-const fs = require('fs');
-
-const stats = (file) => {
-  const stats = fs.statSync(file);
-  const size = (stats.size / 1024).toFixed(2);
-  if (size > 8) {
-    console.error(`${file} - too big: ${size} kb. (8 kb max)`);
-    process.exit(1);
-  }
-  console.info(`${file} - ${size} kb`);
-};
+const { limitSizePlugin } = require('esbuild-plugin-limit-size');
 
 const buildPackage = (pkg) => {
   const options = {
@@ -19,28 +9,21 @@ const buildPackage = (pkg) => {
     minify: true,
     sourcemap: 'linked',
     platform: 'browser',
-    target: 'es2020'
+    target: 'es2020',
+    plugins: [limitSizePlugin(8, true)]
   };
 
   build({
     ...options,
     format: 'cjs',
     outfile: pkg.main
-  })
-    .then(() => {
-      stats(pkg.main);
-    })
-    .catch(() => process.exit(1));
+  }).catch(() => process.exit(1));
 
   build({
     ...options,
     format: 'esm',
     outfile: pkg.module
-  })
-    .then(() => {
-      stats(pkg.module);
-    })
-    .catch(() => process.exit(1));
+  }).catch(() => process.exit(1));
 };
 
 module.exports = { buildPackage };
